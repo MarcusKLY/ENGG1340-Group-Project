50fddfefe7f58d9c68599d77cb3b75cc93fdec2f
Marcus <lykam@connect.hku.hk>
2023-04-03 23:31:42 +0800

    updated story background

--- src/story_background.cpp
+++ src/story_background.cpp
@@ -14,15 +14,19 @@ using namespace std;
 // Print the story background
 int story_background(string player_name)
 {
+    PlayerManager player_manager;
+    player_manager.load_players("saves.sav");
+    PlayerInfo player_info;
+    player_info = player_manager.get_player(player_name);
     char_typewriter("Background:", bold_background_italic_cyan);
-    char_typewriter("You are a HKU student", italic_cyan);
-    char_typewriter("Today you have a lot of assignment waiting for you to finish ", italic_cyan);
-    char_typewriter("So you stayed at KKL building to finish them", italic_cyan);
-    char_typewriter("Unfortunately, you fell asleep...😴 ", italic_cyan);
-    char_typewriter("When you wake up, you notice that it is already 3am on the clock", italic_cyan);
-    char_typewriter("There is no one around you", italic_cyan);
-    char_typewriter("You must go home immediately or your family will scold you", italic_cyan);
-    char_typewriter("However, all the lights are off and the only light source is your phone📱\n\n", italic_cyan);
+    char_typewriter("You are a HKU student", italic_green);
+    char_typewriter("Today you have a lot of assignment waiting for you to finish ", italic_green);
+    char_typewriter("So you stayed at KKL building to finish them", italic_green);
+    char_typewriter("Unfortunately, you fell asleep...😴 ", italic_green);
+    char_typewriter("When you wake up, you notice that it is already 3am on the clock", italic_green);
+    char_typewriter("There is no one around you", italic_green);
+    char_typewriter("You must go home immediately or your family will scold you", italic_green);
+    char_typewriter("However, all the lights are off and the only light source is your phone📱\n\n", italic_green);
     // Call menu function with custom options and question
     bool is_valid = false;
     while (!is_valid)
@@ -37,20 +41,70 @@ int story_background(string player_name)
         {
         case 0:
             // code to handle "Continue sleeping" option
-            char_typewriter("\nYou've slept too long, you can't sleep again!", italic_green);
+            char_typewriter("\nYou've slept too long, you can't sleep again!        ", italic_green);
             clear_previous_lines(6);
             continue;
         case 1:
             // code to handle "Continue staying without leaving" option
-            char_typewriter("\n...................Nothing happens", italic_green);
+            char_typewriter("\n...................Nothing happens             ", italic_green);
             clear_previous_lines(6);
             continue;
             ;
         case 2:
             // code to handle "Take the elevator and leave" option
-            color_print("\nYou stand up and ready to leave\n", italic_green);
+            char_typewriter("\nYou stand up and ready to leave               ", italic_green);
+            is_valid = true;
             break;
         }
-        this_thread::sleep_for(chrono::seconds(2));
+    }
+    char_typewriter("When you are about to leave, you notice that there is a bottle of energy drink on your table\n", italic_green);
+    is_valid = false;
+    while (!is_valid)
+    {
+        vector<string> items;
+        items.push_back("Take it");
+        items.push_back("Just let it go");
+        int choice = choose_event(items, "Are you going to take it?");
+        items.clear(); // Clear the vector
+        switch (choice)
+        {
+        case 0:
+            char_typewriter("\nYou put the drink in your inventory", italic_green);
+            char_typewriter("You then walk to the elevator and head to ground floor", italic_green);
+            player_info.items.push_back("Energy Drink");
+            is_valid = true;
+            break;
+        case 1:
+            char_typewriter("\nYou then walk to the elevator and head to ground floor", italic_green);
+            // code to handle "Continue staying without leaving" option
+            is_valid = true;
+            break;
+        }
+    }
+    char_typewriter("When you reach the ground floor, there is a sign in front of you:\n", italic_green);
+    char_typewriter("  ← CYM Building    ", bold_background_cyan);
+    char_typewriter(" Knowles Building ➡ ", bold_background_cyan);
+    cout << endl;
+    is_valid = false;
+    while (!is_valid)
+    {
+        vector<string> items;
+        items.push_back("Left");
+        items.push_back("Right");
+        int choice = choose_event(items, "Which way do you want to go?");
+        items.clear(); // Clear the vector
+        switch (choice)
+        {
+        case 0:
+            char_typewriter("\nYou put the drink in your inventory", italic_green);
+            player_info.items.push_back("Energy Drink");
+            is_valid = true;
+            break;
+        case 1:
+            // code to handle "Continue staying without leaving" option
+            char_typewriter("\n...................Nothing happens", italic_green);
+            is_valid = true;
+            break;
+        }
     }
 }
