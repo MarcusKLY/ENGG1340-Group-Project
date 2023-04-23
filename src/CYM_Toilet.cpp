8652734286ce61258b887777db2d3bc6b7278b0e
Chuck <lamchakyu05@gmail.com>
2023-04-23 21:01:55 +0800

    asdfghjkl

--- src/CYM_Toilet.cpp
+++ src/CYM_Toilet.cpp
@@ -1,7 +1,7 @@
 // CYM_Toilet.cpp
 
 #include "../header/CYM_Toilet.h"
-#include "../header/choose_event.h"
+//#include "../header/choose_event.h"
 #include "../header/player.h"
 #include "../header/output_style.h"
 #include "../header/word_style.h"
