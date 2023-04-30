69202b1d40f87502d746f3a30ca9dd4553f96eab
Strm <strm@connect.hku.hk>
2023-04-30 15:52:03 +0800

    debugging: password checkpoint, story background c

--- src/story_background.cpp
+++ src/story_background.cpp
@@ -117,11 +117,11 @@ int story_background(string player_name)
         }
     }
     if (player_info.difficulty == "Easy")
-        char_typewriter("There is door locked with english vocabulary that is 4 characters long 🔒\nYou have to guess the password in 10 goes or less, or else the password would be reset\nA correct letter turns green\nA correct letter in the wrong place turns yellow\nAn incorrect letter turns grey", italic_green);
+        char_typewriter("There is door locked with english vocabulary that is 4 characters long 🔒\nYou have to guess the password in 10 goes or less, or else the password would be reset\nA correct letter turns green\nA correct letter in the wrong place turns yellow\nAn incorrect letter turns red", italic_green);
     if (player_info.difficulty == "Normal")
-        char_typewriter("There is door locked with english vocabulary that is 5 characters long 🔒\nYou have to guess the password in 7 goes or less, or else the password would be reset\nA correct letter turns green\nA correct letter in the wrong place turns yellow\nAn incorrect letter turns grey", italic_green);
+        char_typewriter("There is door locked with english vocabulary that is 5 characters long 🔒\nYou have to guess the password in 7 goes or less, or else the password would be reset\nA correct letter turns green\nA correct letter in the wrong place turns yellow\nAn incorrect letter turns red", italic_green);
     if (player_info.difficulty == "Hard")
-        char_typewriter("There is door locked with english vocabulary that is 6 characters long 🔒\nYou have to guess the password in 5 goes or less, or else the password would be reset\nA correct letter turns green\nA correct letter in the wrong place turns yellow\nAn incorrect letter turns grey", italic_green);
+        char_typewriter("There is door locked with english vocabulary that is 6 characters long 🔒\nYou have to guess the password in 5 goes or less, or else the password would be reset\nA correct letter turns green\nA correct letter in the wrong place turns yellow\nAn incorrect letter turns red", italic_green);
     bool pass = 0;
     while (!pass)
     {
