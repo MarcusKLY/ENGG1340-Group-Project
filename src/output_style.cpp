56d4ffb22b66e1f64b45b2a0bac1e5739f2f1e5e
Marcus <lykam@connect.hku.hk>
2023-05-04 00:44:32 +0800

    Finalized for Video Recording

--- src/output_style.cpp
+++ src/output_style.cpp
@@ -13,14 +13,14 @@ using namespace std;
 // animated cout function which print the string character by character
 int char_typewriter(string s, ColorOption colorOptions)
 {
-    chrono::milliseconds timespan(2); // 200
+    chrono::milliseconds timespan(200); // 200
     this_thread::sleep_for(timespan);
     string word = s;
     cout << colorOptions.code;
     for (char c : word)
     {
         cout << c << flush;
-        chrono::milliseconds timespan(2); // 200
+        chrono::milliseconds timespan(20); // 20
         this_thread::sleep_for(timespan);
     }
     cout << "\033[0m" << endl; // Reset color to default
@@ -30,7 +30,7 @@ int char_typewriter(string s, ColorOption colorOptions)
 // animated cout function which print the string line with waiting time
 int line_typewriter(string s, ColorOption colorOptions)
 {
-    chrono::milliseconds timespan(2);
+    chrono::milliseconds timespan(200); // 200
     this_thread::sleep_for(timespan);
     string word = s;
     cout << colorOptions.code;
